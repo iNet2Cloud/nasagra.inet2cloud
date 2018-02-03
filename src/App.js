c5b8740b4e3eae453039cc9e5d4aaa735b77049b
Sameer Sharma <pradeep.bhargava@inet2cloud.com>
2018-02-03 17:54:51 +0530

    changing logo and message

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './iNet2Cloud.jpg';
 import './App.css';
 
 class App extends Component {
@@ -11,7 +11,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Building Education Market Place only for Competitive Exams...
         </p>
       </div>
     );
